469fc193d9a10e10a0b47366baf54b6e02b2b515
manjulayc <42584747+manjulayc@users.noreply.github.com>
2020-11-03 22:47:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PromotionEngine;
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total Number of Products");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Cart cart = new Cart();
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Enter the Type of products:A,B,C or D");
+                string type = Console.ReadLine();
+                Product p = new Product(type);
+                cart.AddProduct(p);
+            }
+           
+            int totalPrice = cart.GetPrice();
+            Console.WriteLine(totalPrice);
+            Console.ReadLine();
+        }
+
+    }
+}
