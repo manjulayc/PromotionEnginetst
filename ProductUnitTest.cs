14a801504236652dd2f5924fd566994e12db6914
manjulayc <42584747+manjulayc@users.noreply.github.com>
2020-11-03 21:58:24 +0530

    Add files via upload

---
+++ ProductUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine;
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class ProductUnitTest
+    {
+        [TestMethod]
+        public void Product_Price_Returns_Empty_When_Type_Is_Empty()
+        {
+            //Arrange
+            string type = "";
+            decimal price = 0;
+            Product p = new Product(type);
+
+            //Act
+            price = p.Price;
+
+            //Assert
+            Assert.AreEqual(price, 0);
+            Assert.AreNotEqual(price, 30);
+        }
+
+        [TestMethod]
+        public void Product_TypeA_Returns_Price_50()
+        {
+            //Arrange
+            string type = "A";
+            decimal price = 0;
+            Product p = new Product(type);
+
+            //Act
+            price = p.Price;
+
+            //Assert
+            Assert.AreEqual(price, 50);
+        }
+
+        [TestMethod]
+        public void Product_Typeb_smallcase_Returns_Price_0_()
+        {
+            //Arrange
+            string type = "b";
+            decimal price = 0;
+            Product p = new Product(type);
+
+            //Act
+            price = p.Price;
+
+            //Assert
+            Assert.AreEqual(price, 0);
+        }
+    }
+}
